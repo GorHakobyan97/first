d2643cb7d11fd047183fa5baadbb8bf3df5229bc
arman <arman.antonyan12@gmail.com>
2018-03-16 14:48:13 +0400

    ....

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,15 @@ $(document).ready(function(){
 		var frTop2 = $('#third').offset().top;
 	var lastScrollTop = 0;
 	var lastScrollTop2 = 0;
+
 	$(window).on('scroll',function(){
+		
 		var top = $(window).scrollTop();
 		var thtop = parseInt($('#third').css('top'));
 		var right = parseInt($('#second').css('right'));
-		var right2 = parseInt($('#for').css('right'));
+		var right2 = parseInt($('#four').css('right'));
 		var difference = 50;
+		console.log(top+'-----'+frTop)
 		if(top > frTop ){
 			$('#first').css({top:0,position:'fixed'});
 			if (top > lastScrollTop){
@@ -22,72 +25,54 @@ $(document).ready(function(){
 					}
 					$('#second').css('right', right + difference);
 					
-					
-				
-
 			} else
 					if(top<frTop+x){
-						$('#second').css('right', right - difference+-50);
-					
-			
-					}
-				
-					if(right==0){
-						
+						$('#second').css('right', right - difference+-20);
 							
-						    $('#third').css('top', 2*x);
-						  
-   				
-   				y=50;
+					}
+					if(right==0){
+					
+						    $('#third').css('top', 2.5*x);	  	
+   				y=20;
    				if(top>=lastScrollTop2){
-
 						 if(top>x*4){
-						   	
 						  if(Math.abs(right2) <= y){
 								 y=Math.abs(right2);
 					}				
-						    	$('#for').css('right', right2 + y);
+						    	$('#four').css('right', right2 + y);
 						    	}
 						   else{
 							  
-						   	$('#for').css('right',"-100%");
+						   	$('#four').css('right',"-100%");
 						   }
 						    	}
 						else {
-							console.log(x);
 							
-							if(top<frTop2-x*6.8){
-							$('#for').css('right', right2 - y+-50);
-							}
+							
+							
+							$('#four').css('right', right2 - y+-20);
+							
 							
 						}
-						
-						    
-
-  
 						}
-
 						    else{
 						    	$('#third').css('top', '100%');
-						    	
-						    }
-   				
-
-			
-				
+						    	   }
 			lastScrollTop = top;
-
-
 		lastScrollTop2 = top;
 		}else{
 			$('#first').css({position:'relative'});
-			$('#second').css('right',"-100%");
+			$('#second').css('right',"-150%");
 		}
 
+			if(top==0){
+				$('#four').css('right', '100%')
+			}
 
+			if(right2==0){
+				$('#third').css('top', '-100%');
+			}
 	});
-//:))))))))))))))))))))))))))))):?
 
 });
-
 
